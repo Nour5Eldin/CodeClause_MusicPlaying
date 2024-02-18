14187c0344cc0c8d12f5022fcdbcf3ac9d8d1117
noureldinmahmoud <noureldinmahmoud02@gmail.com>
2024-02-18 22:12:00 +0200

    My application

--- app/src/main/java/com/noureldin/musicplaying/ui/fragment/MyApplication.kt
+++ app/src/main/java/com/noureldin/musicplaying/ui/fragment/MyApplication.kt
@@ -1,12 +1,12 @@
 package com.noureldin.musicplaying.ui.fragment
 
 import android.app.Application
-import com.noureldin.musicplaying.service.SongService
+
 
 class MyApplication : Application() {
 
     val musicPlayerManager: MusicPlayerManager by lazy { MusicPlayerManager() }
-    lateinit var songService: SongService
+
 
     companion object{
 
@@ -23,7 +23,6 @@ class MyApplication : Application() {
         super.onCreate()
 
         instance = this
-        songService = SongService()
 
 
 
